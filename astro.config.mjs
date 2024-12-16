b8952ad19b015c49fe5e74af935cfc1936c7bbd2
Mario Chico <chico.developer@hotmail.com>
2024-12-15 18:09:30 -0600

    Agregando esquema de tokens de notion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      NOTION_API_TOKEN:envField.string ({
+       access: 'secret',
+       context: 'server', 
+      }),
+      NOTION_DATABASE_ID:envField.string ({
+       access: 'public',
+       context: 'client', 
+      }),
+      NOTION_DATABASE_ID_EXAMPLE:envField.string ({
+        access: 'public',
+        context: 'client', 
+       }),
+      NOTION_API_TOKEN_EXAMPLE:envField.string ({
+        access: 'secret',
+        context: 'server', 
+      }),
+    },
+  },
+});
